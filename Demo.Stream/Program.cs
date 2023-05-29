bd41e70b8b0d066a8e1ff9b21ef9ca08b0388b48
LiteObject <Lite.Object@gmail.com>
2023-05-28 21:40:53 -0500

    minor refactors

--- Demo.Stream/Program.cs
+++ Demo.Stream/Program.cs
@@ -8,17 +8,15 @@ namespace Demo.Stream
         static void Main(string[] args)
         {
             string filePath = "MyFiel.txt";
-            File.WriteAllText(filePath, "Hello World!");
+            File.WriteAllText(filePath, $"[{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] Hello World!");
 
-            byte[] bytes = Encoding.ASCII.GetBytes("This is BufferedStream demo.");
+            byte[] bytes = Encoding.ASCII.GetBytes($"\n[{DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] This is a BufferedStream demo!");
 
-            using (FileStream fileStream = new(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                BufferedStream bufferedStream = new(fileStream, 1024);
-                bufferedStream.Write(bytes);
-                bufferedStream.Flush();
-                bufferedStream.Close();
-            }
+            using FileStream fileStream = new(filePath, FileMode.Append, FileAccess.Write);
+            BufferedStream bufferedStream = new(fileStream, 1024);
+            bufferedStream.Write(bytes);
+            bufferedStream.Flush();
+            bufferedStream.Close();
 
             //using (MemoryStream memoryStream = new(50))
             //{
